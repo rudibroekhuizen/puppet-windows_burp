81f82b42eddd49e5e7d5cc4390beb513abaff792
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-03-10 20:22:33 +0100

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -3,16 +3,25 @@
 #
 class windows_burp::package {
 
-  download_file { "burp-win64-installer-1.4.32.exe":
-    url                   => 'http://downloads.sourceforge.net/project/burp/burp-1.4.32/burp-win64-installer-1.4.32.exe',
+  case $architecture {
+    'x64': { 
+      $bit = 'win64'
+	  }
+    '32': {
+  	  $bit = 'win32'
+  	}
+  }
+
+  download_file { "burp-${bit}-installer-1.4.32.exe":
+    url                   => 'http://downloads.sourceforge.net/project/burp/burp-1.4.32/burp-${bit}-installer-1.4.32.exe',
     destination_directory => 'c:\temp'
   }
 
   package { 'burp':
-    source  => 'c:\temp\burp-win64-installer-1.4.32.exe',
+    source  => 'c:\temp\burp-${bit}-installer-1.4.32.exe',
     ensure  => present,
     install_options => ['/S'],
-    require => Download_file [ 'burp-win64-installer-1.4.32.exe' ],
+    require => Download_file [ 'burp-${bit}-installer-1.4.32.exe' ],
   }
 
 }
