8b4d40eaa0e71e11e771af9222d7c04e9ea28522
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-03-10 19:50:43 +0100

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -3,10 +3,9 @@ Class: windows_burp::package
 #
 class windows_burp::package {
 
-  download_file { "burp-win64-installer-1.4.32.exe" :
+  download_file { "burp-win64-installer-1.4.32.exe":
     url                   => 'http://downloads.sourceforge.net/project/burp/burp-1.4.32/burp-win64-installer-1.4.32.exe?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fburp%2Ffiles%2Fburp-1.4.32%2Fburp-win64-installer-1.4.32.exe%2Fdownload&ts=1426007464&use_mirror=softlayer-ams',
     destination_directory => 'c:/temp'
   }
-
 
 }
