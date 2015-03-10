f6db17198bd6ba51cff5621e28600148f4214226
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-03-10 11:42:51 +0100

    Update package.pp

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,7 +2,4 @@ Class: windows_burp::package
 #
 #
 class windows_burp::package {
-
-  if $::operatingsystem != 'Windows' {
-    notice( 'This module is designed for Windows, for Linux please user module puppet module puppet-burp.' )
   }
