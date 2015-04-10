0251ceded16a5f2f6a1831196d2deada3a66b121
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-04-10 16:57:23 +0200

    Update client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -2,3 +2,12 @@
 #
 #
 class windows_burp::client {
+
+  # Configure burp.conf client config file
+  file { 'C:/Program Files/Burp/burp.conf':
+    ensure  => present,
+    content => template("burp/burp.conf.erb"),
+    require => Class['windows_burp::package']
+  }
+
+}
