6f41506864d7fd1c3a62be7f0aa4aa591fd573e3
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-07-13 19:52:39 +0200

    Update client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,13 +1,8 @@
 # Class: windows_burp::client
 #
-#
 class windows_burp::client {
 
-  # Configure burp.conf client config file
-  file { 'C:/Program Files/Burp/burp.conf':
-    ensure  => present,
-    content => template("burp/burp.conf.erb"),
-    require => Class['windows_burp::package']
-  }
+  # Set settings in C:/Program Files/Burp/burp.conf
+  create_resources( 'windows_burp::defines::burp', $windows_burp::burp_hash )
 
 }
