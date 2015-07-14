68c0c2fe5865c6a7dd314ac910fe0768cd237efc
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-07-14 19:17:27 +0200

    Update client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -5,4 +5,12 @@ class windows_burp::client {
   # Set settings in C:/Program Files/Burp/burp.conf
   create_resources( 'windows_burp::defines::burp', $windows_burp::burp_hash )
 
+  # Set cname as fqdn
+  ini_setting { "C:/Program Files/Burp/burp.conf":
+    ensure  => present,
+    path    => "C:/Program Files/Burp/burp.conf",
+    setting => cname,
+    value   => $fqdn,
+  }
+
 }
